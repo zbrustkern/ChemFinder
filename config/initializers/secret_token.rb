8e3db2152bc7eeb62bbcc508622e3f110af6c967
Zeke Brustkern <zbrustkern@gmail.com>
2012-02-20 11:29:49 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChemFinder::Application.config.secret_token = '6a0ec5d188e7529d1c4993b237e3e3d88b5d12c554b9d3a614250b7bbace51e3e558895e28428c9fac524be3ef040f702f28cde88383a6ce1c13b412dcec22d9'
