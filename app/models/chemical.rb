d7d42ffa16faf5579cbed8ff2a90e0ad6997716a
Zeke Brustkern <zbrustkern@gmail.com>
2012-02-22 20:40:04 -0500

    home link working

--- app/models/chemical.rb
+++ app/models/chemical.rb
@@ -1,5 +1,5 @@
 class Chemical < ActiveRecord::Base
-#  has_and_belongs_to_many :fun_groups
+#has_and_belongs_to_many :fun_groups
   
   validates :name, :presence => true, :uniqueness => { :case_sensitive => false }
   validates :casnum, :uniqueness => true
